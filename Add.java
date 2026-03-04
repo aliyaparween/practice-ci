905702afe8859c3dd5d88246e3d9a7e4fb92b772
Aliya Parween <136161736+aliyaparween@users.noreply.github.com>
2026-03-04 08:39:48 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,7 +1,7 @@
 public class Add{
   public static void main(String[] args){
     int num1=10;
-    int num2=20;
+    int num2=30;
     int sums=num1+num2;
     System.out.println("Sum of "+num1 +" & "+num2+"is : "+sums);
   }
